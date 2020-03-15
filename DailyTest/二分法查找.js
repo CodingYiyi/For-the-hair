74dfad5cd0c38d0df19e1ba3bbd18c63c341809c
张毅 <zhangyi@tseveryday.com>
2020-03-15 23:40:57 +0800

    dayli codding

---
+++ DailyTest/二分法查找.js
@@ -0,0 +1,15 @@
+// 给定一个数组，和一个待查找的数字target，使用二分法查找的方式返回查找的下标，没找到的话返回-1
+// 二分法查找的前提是：数组有序！
+function middleSearch (source,target){
+    let startIndex=0,endIndex=source.length-1
+    while(startIndex <= endIndex){
+        let middleIndex = Math.floor((startIndex+endIndex)/2)
+        if(source[middleIndex] === target) return middleIndex
+        else if(source[middleIndex]<target) startIndex = middleIndex+1
+        else endIndex = middleIndex-1
+    }
+    return -1
+}
+
+let source = [2,4,6,8,9,10]
+console.log(middleSearch(source,11))
