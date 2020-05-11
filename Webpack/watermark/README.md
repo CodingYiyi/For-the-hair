f0733b4c7238fc337ecdfa5f0e36bf7e2025551e
张毅 <zhangyi@tseveryday.com>
2020-05-11 10:25:26 +0800

    daily test

---
+++ Webpack/watermark/README.md
@@ -0,0 +1,6 @@
+## webpack 调试（归根结底是调试node的过程）
+
+https://www.webpackjs.com/contribute/debugging/
+
+https://medium.com/@jsilvax/debugging-webpack-with-vs-code-b14694db4f8e
+
---
+++ Webpack/watermark/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+debugger
+const getLoaderPath = (loaderPath) => {
+    return path.resolve(__dirname, "loaders", loaderPath);
+}
+
+module.exports  = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: getLoaderPath("loader1"),
+                        options: {
+                            author: 'laibao101',
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg)$/,
+                use: [
+                    {
+                        loader: getLoaderPath("loader2"),
+                        options: {
+                            watermarkPath: "./static/img/mi-logo.png",
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
